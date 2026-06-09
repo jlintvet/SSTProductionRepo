8dbbe5d35e0e5e7fb4ce825766f8848349529860
Jon Lintvet <jlintvet@gmail.com>
2026-06-09 16:40:53 +0000

    feat: tie OceanCityMD wrecks to Ocean City Inlet departure location

--- src/config/regionConfig.js
+++ src/config/regionConfig.js
@@ -18,7 +18,7 @@ export const REGION_CONFIGS = {
       { label: "Bay Bridge Tunnel, VA", lat: 36.9082,            lon: -76.0918,           wreckRegion: "ChesapeakeMD", noaaCoverage: true  },
       { label: "Beaufort Inlet, NC",    lat: 34.6937,            lon: -76.6663,           wreckRegion: "MoreheadNC",   noaaCoverage: true  },
       { label: "Hatteras Inlet, NC",    lat: 35.190505871104094, lon: -75.7554186087437,  wreckRegion: "HatterasNC",   noaaCoverage: true  },
-      { label: "Ocean City Inlet, MD",  lat: 38.324,             lon: -75.0883,           wreckRegion: "ChesapeakeMD", noaaCoverage: true  },
+      { label: "Ocean City Inlet, MD",  lat: 38.324,             lon: -75.0883,           wreckRegion: "OceanCityMD",  noaaCoverage: true  },
       { label: "Oregon Inlet, NC",      lat: 35.7792,            lon: -75.532,            wreckRegion: "HatterasNC",   noaaCoverage: true  },
       { label: "Poquoson, VA",          lat: 37.7629068,         lon: -75.7724311,        wreckRegion: "ChesapeakeMD", noaaCoverage: true  },
       { label: "Virginia Beach, VA",    lat: 36.8516,            lon: -75.9792,           wreckRegion: "ChesapeakeMD", noaaCoverage: true  },
